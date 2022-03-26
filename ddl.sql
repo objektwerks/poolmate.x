b105a27641bd9aadf2271a3d013c17136ced5a77
objektwerks <objektwerks@runbox.com>
2022-03-26 08:51:52 -0400

    added cost to timer table

--- ddl.sql
+++ ddl.sql
@@ -47,7 +47,8 @@ CREATE TABLE timer (
   id BIGSERIAL PRIMARY KEY,
   pool_id BIGINT REFERENCES pool(id),
   installed INT NOT NULL,
-  model VARCHAR NOT NULL
+  model VARCHAR NOT NULL,
+  cost INT NOT NULL
 );
 
 CREATE TABLE timer_setting (
