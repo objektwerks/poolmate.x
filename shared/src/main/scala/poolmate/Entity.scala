7461b6378587d53bce287d693854d0068f8fa0dc
objektwerks <objektwerks@runbox.com>
2022-03-26 08:48:55 -0400

    added cost to timer

--- shared/src/main/scala/poolmate/Entity.scala
+++ shared/src/main/scala/poolmate/Entity.scala
@@ -91,7 +91,8 @@ final case class Pump(id: Long = 0,
 final case class Timer(id: Long = 0,
                        poolId: Long = 0,
                        installed: Int = 0,
-                       model: String = "") extends Entity:
+                       model: String = "",
+                       cost: Int = 0) extends Entity:
   def display = model
 
 final case class TimerSetting(id: Long = 0,
