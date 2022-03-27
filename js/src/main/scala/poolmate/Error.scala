dd4a782de6d025e5605b91cc6ac87cee66e4e69d
objektwerks <objektwerks@runbox.com>
2022-03-27 08:31:16 -0400

    edited pin error

--- js/src/main/scala/poolmate/Error.scala
+++ js/src/main/scala/poolmate/Error.scala
@@ -2,7 +2,7 @@ package poolmate
 
 object Error:
   val emailAddressError = "Enter a valid email address."
-  val pinError = "Enter a valid pin of 6 alphanumeric characters."
+  val pinError = "Enter a valid pin of 7 alphanumeric characters."
 
   val nameError = "Enter a 2 characters or more."
   val builtError = "Enter a year greater than 1900."
