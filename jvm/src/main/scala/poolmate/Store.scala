b3b8922ff9e48ac7a0df8038ee7ed26c77631449
objektwerks <objektwerks@runbox.com>
2022-03-27 08:15:09 -0400

    added cost to store deck

--- jvm/src/main/scala/poolmate/Store.scala
+++ jvm/src/main/scala/poolmate/Store.scala
@@ -173,20 +173,20 @@ final class Store(conf: Config,
   def listDecks(): List[Deck] =
     DB readOnly { implicit session =>
       sql"select * from deck order by installed desc"
-        .map(rs => Deck(rs.long("id"), rs.long("pool_id"), rs.int("installed"), rs.string("kind")))
+        .map(rs => Deck(rs.long("id"), rs.long("pool_id"), rs.int("installed"), rs.string("kind"), rs.int("cost")))
         .list()
     }
 
   def addDeck(deck: Deck): Deck =
     val id = DB localTx { implicit session =>
-      sql"insert into deck(pool_id, installed, kind) values(${deck.poolId}, ${deck.installed}, ${deck.kind})"
+      sql"insert into deck(pool_id, installed, kind, cost) values(${deck.poolId}, ${deck.installed}, ${deck.kind}, ${deck.cost})"
       .updateAndReturnGeneratedKey()
     }
     deck.copy(id = id)
 
   def updateDeck(deck: Deck): Unit =
     DB localTx { implicit session =>
-      sql"update deck set installed = ${deck.installed}, kind = ${deck.kind} where id = ${deck.id}"
+      sql"update deck set installed = ${deck.installed}, kind = ${deck.kind}, cost = ${deck.cost} where id = ${deck.id}"
       .update()
     }
     ()
