5e2069f60ca27a63b08ff23d3dbab8d738d93020
objektwerks <objektwerks@runbox.com>
2022-03-27 08:32:41 -0400

    edited is pin validator

--- shared/src/main/scala/poolmate/Validators.scala
+++ shared/src/main/scala/poolmate/Validators.scala
@@ -4,7 +4,7 @@ object Validators:
   extension (value: String)
     def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
     def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
-    def isPin: Boolean = value.length == 6
+    def isPin: Boolean = value.length == 7
     def isName: Boolean = value.length >= 2
 
   extension (value: Int)
