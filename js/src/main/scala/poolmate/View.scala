64d3e6f16464acb32ce239a9a405f7becc12324e
objektwerks <objektwerks@runbox.com>
2023-02-15 13:51:07 -0500

    refactored view

--- js/src/main/scala/poolmate/View.scala
+++ js/src/main/scala/poolmate/View.scala
@@ -7,7 +7,7 @@ import org.scalajs.dom.console
 trait View:
   protected[this] val errorBus = new EventBus[String]
 
-  def call(command: Command, handler: (event: Either[Fault, Event]) => Unit): Unit = Proxy.call(command, handler)
+  def call(command: Command, handler: Event => Unit): Unit = Proxy.call(command, handler)
   
   def route(page: Page): Unit = PageRouter.router.pushState(page)
 
