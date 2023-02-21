7ec8a94f0f1fde545066f90528621ba088eddbbb
objektwerks <objektwerks@runbox.com>
2023-02-21 12:47:37 -0500

    removed unused imports

--- js/src/main/scala/poolmate/RegisterView.scala
+++ js/src/main/scala/poolmate/RegisterView.scala
@@ -2,11 +2,6 @@ package poolmate
 
 import com.raquo.laminar.api.L.*
 
-import org.scalajs.dom.console.log
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
 import Components.*
 import Error.*
 import Message.*
