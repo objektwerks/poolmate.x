d70d0447cef7c0306ab1c158b44ed105d4b4646d
objektwerks <objektwerks@runbox.com>
2023-02-21 12:55:03 -0500

    simplified register view

--- js/src/main/scala/poolmate/RegisterView.scala
+++ js/src/main/scala/poolmate/RegisterView.scala
@@ -9,8 +9,6 @@ import Validators.*
 
 object RegisterView extends View:
   def apply(emailAddressVar: Var[String], pinVar: Var[String], accountVar: Var[Account]): HtmlElement =
-    val emailAddressErrorBus = new EventBus[String]
-
     def handler(event: Event): Unit =
       event match
         case Fault(_, _, _, cause) => emitError(s"Register failed: $cause")
@@ -30,11 +28,7 @@ object RegisterView extends View:
       email.amend {
         value <-- emailAddressVar
         onInput.mapToValue.filter(_.nonEmpty).setAsValue --> emailAddressVar
-        onKeyUp.mapToValue --> { emailAddress =>
-          if emailAddress.isEmailAddress then clear(emailAddressErrorBus) else emit(emailAddressErrorBus, emailAddressError)
-        }
       },
-      err(emailAddressErrorBus),
       cbar(
         btn("Register").amend {
           disabled <-- emailAddressVar.signal.map(email => !email.isEmailAddress)
