9ba6354e2743bab71ecb4fa16998b59128b3e4c6
objektwerks <objektwerks@runbox.com>
2023-02-21 13:18:06 -0500

    refactored Accoutn

--- shared/src/main/scala/poolmate/Entity.scala
+++ shared/src/main/scala/poolmate/Entity.scala
@@ -22,42 +22,12 @@ sealed trait Entity:
   def display: String
 
 final case class Account(id: Long = 0,
-                         license: String = newLicense,
+                         license: String = "",
                          emailAddress: String,
-                         pin: String = newPin,
+                         pin: String = "",
                          activated: Int = DateTime.currentDate,
                          deactivated: Int = 0) extends Entity:
   def display = emailAddress
-
-object Account:
-  private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
-  private val random = new Random
-
-  private def newSpecialChar: Char = specialChars(random.nextInt(specialChars.length))
-
-  /**
-   * 26 letters + 10 numbers + 18 special characters = 54 combinations
-   * 7 alphanumeric char pin = 54^7 ( 1,338,925,209,984 )
-   */
-  private def newPin: String =
-    Random.shuffle(
-      Random
-        .alphanumeric
-        .take(5)
-        .mkString
-        .prepended(newSpecialChar)
-        .appended(newSpecialChar)
-    ).mkString
-
-  private def newLicense: String = UUID.randomUUID.toString
-
-  val empty = Account(
-    license = "",
-    emailAddress = "",
-    pin = "",
-    activated = 0,
-    deactivated = 0
-  )
 
 final case class Pool(id: Long = 0,
                       license: String = "",
