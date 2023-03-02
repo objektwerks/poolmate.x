8cc3f8cd9b96ada1a6047722639567609dee737c
objektwerks <objektwerks@runbox.com>
2023-03-02 10:53:35 -0500

    added measurements view

---
+++ js/src/main/scala/poolmate/MeasurementsView.scala
@@ -0,0 +1,53 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+import Validator.*
+
+object MeasurementsView extends View:
+  def apply(poolId: Long, model: Model[Measurement], license: String): HtmlElement =
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case MeasurementsListed(measurements: List[Measurement]) =>
+          clearErrors()
+          model.setEntities(measurements)
+        case _ => log(s"Measurements -> handler failed: $event")
+
+    div(
+      bar(
+        btn("Pool").amend {
+          onClick --> { _ =>
+            log("Cleanings -> Pool menu item onClick")
+            route(PoolPage()) // pool id
+          }
+        }      
+      ),
+      div(
+        onLoad --> { _ => 
+          val command = ListMeasurements(license, poolId)
+          call(command, handler)
+        },
+        hdr("Measurements"),
+        err(errorBus),
+        listview(
+          split(model.entitiesVar, (id: Long) => MeasurementPage(id))
+        )
+      ),
+      cbar(
+        btn("New").amend {
+          onClick --> { _ =>
+            log(s"Measurements -> New button onClick")
+            route(MeasurementPage())
+          }
+        },        
+        btn("Refresh").amend {
+          onClick --> { _ =>
+            log(s"Measurements -> Refresh button onClick")
+            val command = ListMeasurements(license, poolId)
+            call(command, handler)
+          }
+        }
+      )
+    )
