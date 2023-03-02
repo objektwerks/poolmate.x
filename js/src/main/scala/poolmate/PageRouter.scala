143229ac3930881836a265d8b8efe614415677de
objektwerks <objektwerks@runbox.com>
2023-03-02 11:32:06 -0500

    added measurements to page router

--- js/src/main/scala/poolmate/PageRouter.scala
+++ js/src/main/scala/poolmate/PageRouter.scala
@@ -68,6 +68,8 @@ object PageRouter:
     .collectStatic(AccountPage) { AccountView(Model.accountVar) }
     .collectStatic(PoolsPage) { PoolsView(Model.pools, Model.accountVar) }
     .collect[PoolPage] { page => PoolView(Model.pools.setSelectedEntityById(page.id), Model.accountVar) }
+    .collectStatic(MeasurementsPage) { MeasurementsView(Model.pools.selectedEntityVar.now().id, Model.measurements, Model.license) }
+    .collect[MeasurementPage] { page => MeasurementView(Model.measurements.setSelectedEntityById(page.id), Model.license) }
     .collectStatic(CleaningsPage) { CleaningsView(Model.cleanings.selectedEntityVar.now().id, Model.cleanings, Model.license) }
     .collect[CleaningPage] { page => CleaningView(Model.cleanings.setSelectedEntityById(page.id), Model.license) }
     .collectStatic(ChemicalsPage) { ChemicalsView(Model.pools.selectedEntityVar.now().id, Model.chemicals, Model.license) }
