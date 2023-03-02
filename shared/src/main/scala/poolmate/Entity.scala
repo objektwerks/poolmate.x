da49392d93ed0c354052ba8942b1b3679bdbdb20
objektwerks <objektwerks@runbox.com>
2023-03-02 11:11:04 -0500

    added unit to pool entity

--- shared/src/main/scala/poolmate/Entity.scala
+++ shared/src/main/scala/poolmate/Entity.scala
@@ -67,6 +67,7 @@ final case class Pool(id: Long = 0,
                       name: String = "",
                       built: Long = 0,
                       volume: Int = 1000,
+                      unit: String = UnitOfMeasure.gl.toString,
                       cost: Int = 0) extends Entity:
   def display = name
 
