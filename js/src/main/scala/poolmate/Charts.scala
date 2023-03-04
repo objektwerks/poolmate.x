227ee63087f6d4719e12f78e7f91c02318413e4e
objektwerks <objektwerks@runbox.com>
2023-03-04 12:39:33 -0500

    added charts

---
+++ js/src/main/scala/poolmate/Charts.scala
@@ -0,0 +1,15 @@
+package poolmate
+
+import scala.scalajs.js
+import scala.scalajs.js.JSConverters.*
+
+import com.raquo.laminar.api.L.{*, given}
+
+import typings.chartJs.mod.*
+
+/**
+ * Chart.js, via ScalaJs, is not exactly ready
+ * for prime time (2023.2.25). Try again later.
+ */
+object Charts:  
+  def apply(): HtmlElement = ???
