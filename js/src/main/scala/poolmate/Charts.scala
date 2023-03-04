d73b741f7c606bbe8ff950a6c0bd2eef3a8be749
objektwerks <objektwerks@runbox.com>
2023-03-04 12:42:49 -0500

    edited charts

--- js/src/main/scala/poolmate/Charts.scala
+++ js/src/main/scala/poolmate/Charts.scala
@@ -5,8 +5,6 @@ import scala.scalajs.js.JSConverters.*
 
 import com.raquo.laminar.api.L.{*, given}
 
-import typings.chartJs.mod.*
-
 /**
  * Chart.js, via ScalaJs, is not exactly ready
  * for prime time (2023.2.25). Try again later.
