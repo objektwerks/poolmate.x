b9823d9bf2dd310067389ae9600c44a21ca2ebad
objektwerks <objektwerks@runbox.com>
2023-03-05 07:42:24 -0500

    added supplies view

---
+++ js/src/main/scala/poolmate/SuppliesView.scala
@@ -0,0 +1,14 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+import Validator.*
+
+object SuppliesView extends View:
+  def apply(poolId: Long, model: Model[Supply], license: String): HtmlElement = ???
+    def handler(event: Event): Unit = ???
+
+    div(
+      
+    )
