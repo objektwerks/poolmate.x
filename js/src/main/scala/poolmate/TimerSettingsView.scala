84467f1834d349b93fb3a12e5eb951f8cbebad34
objektwerks <objektwerks@runbox.com>
2023-03-05 07:39:57 -0500

    added timer settings view

---
+++ js/src/main/scala/poolmate/TimerSettingsView.scala
@@ -0,0 +1,14 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+import Validator.*
+
+object TimerSettingsView  extends View:
+  def apply(poolId: Long, model: Model[TimerSetting], license: String): HtmlElement = ???
+    def handler(event: Event): Unit = ???
+
+    div(
+      
+    )
