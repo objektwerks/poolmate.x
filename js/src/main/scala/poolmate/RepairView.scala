673c2ce9cf536c53e056b054c731a05af22e417c
objektwerks <objektwerks@runbox.com>
2023-03-05 07:57:40 -0500

    added repair view

---
+++ js/src/main/scala/poolmate/RepairView.scala
@@ -0,0 +1,16 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+import Validator.*
+
+object RepairView extends View:
+  def apply(model: Model[Repair], license: String): HtmlElement =
+    def addHandler(event: Event): Unit = ???
+
+    def updateHandler(event: Event): Unit = ???
+
+    div(
+      
+    )
