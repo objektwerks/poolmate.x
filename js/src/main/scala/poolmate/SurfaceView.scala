2877e279e1be487a17b9f958d3e0e60e00dfe6c0
objektwerks <objektwerks@runbox.com>
2023-03-05 07:49:47 -0500

    added surface view

---
+++ js/src/main/scala/poolmate/SurfaceView.scala
@@ -0,0 +1,14 @@
+package poolmate
+
+import com.raquo.laminar.api.L.*
+
+import Component.*
+import Validator.*
+
+object SurfaceView extends View:
+  def apply(model: Model[Supply], license: String): HtmlElement =
+    def handler(event: Event): Unit = ???
+
+    div(
+      
+    )
