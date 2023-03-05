6f9a63d84486b4086342497ed19b2f7a13a509de
objektwerks <objektwerks@runbox.com>
2023-03-05 07:51:49 -0500

    added deck view

--- js/src/main/scala/poolmate/DeckView.scala
+++ js/src/main/scala/poolmate/DeckView.scala
@@ -5,8 +5,8 @@ import com.raquo.laminar.api.L.*
 import Component.*
 import Validator.*
 
-object SurfaceView extends View:
-  def apply(model: Model[Supply], license: String): HtmlElement =
+object DeckView extends View:
+  def apply(model: Model[Deck], license: String): HtmlElement =
     def addHandler(event: Event): Unit = ???
 
     def updateHandler(event: Event): Unit = ???
