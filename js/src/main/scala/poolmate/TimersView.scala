1530b9107b79606d3498e77c1f05b78eeb7baee7
objektwerks <objektwerks@runbox.com>
2023-03-12 08:26:51 -0400

    added handler to timers view

--- js/src/main/scala/poolmate/TimersView.scala
+++ js/src/main/scala/poolmate/TimersView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object TimersView extends View:
   def apply(poolId: Long, model: Model[Timer], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case TimersListed(timers: List[Timer]) =>
+          clearErrors()
+          model.setEntities(timers)
+        case _ => log(s"Timers -> handler failed: $event")
 
     div(
       
