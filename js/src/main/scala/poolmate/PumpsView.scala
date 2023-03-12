b8be6281cd85c1c9cce45a91e88af07d747f0692
objektwerks <objektwerks@runbox.com>
2023-03-12 08:25:13 -0400

    added div to pumps view

--- js/src/main/scala/poolmate/PumpsView.scala
+++ js/src/main/scala/poolmate/PumpsView.scala
@@ -15,5 +15,38 @@ object PumpsView extends View:
         case _ => log(s"Pumps -> handler failed: $event")
 
     div(
-      
+      bar(
+        btn("Pool").amend {
+          onClick --> { _ =>
+            log("Pumps -> Pool menu item onClick")
+            route(PoolPage(poolId))
+          }
+        }      
+      ),
+      div(
+        onLoad --> { _ => 
+          val command = ListPumps(license, poolId)
+          call(command, handler)
+        },
+        hdr("Pumps"),
+        err(errorBus),
+        listview(
+          split(model.entitiesVar, (id: Long) => PumpPage(id))
+        )
+      ),
+      cbar(
+        btn("New").amend {
+          onClick --> { _ =>
+            log(s"Pumps -> New button onClick")
+            route(PumpPage(poolId, model.selectedEntityVar.now().id))
+          }
+        },        
+        btn("Refresh").amend {
+          onClick --> { _ =>
+            log(s"Pumps -> Refresh button onClick")
+            val command = ListPumps(license, poolId)
+            call(command, handler)
+          }
+        }
+      )
     )
