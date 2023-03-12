849e42de5f616c7c75b001e3d73345d6d24d3068
objektwerks <objektwerks@runbox.com>
2023-03-12 08:20:30 -0400

    removed import

--- js/src/main/scala/poolmate/DecksView.scala
+++ js/src/main/scala/poolmate/DecksView.scala
@@ -3,7 +3,6 @@ package poolmate
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Validator.*
 
 object DecksView extends View:
   def apply(poolId: Long, model: Model[Deck], license: String): HtmlElement =
