c51561a5b74d7b25f74140a5e3cbdc34638a3e54
objektwerks <objektwerks@runbox.com>
2023-03-12 08:17:23 -0400

    added handler to decks view

--- js/src/main/scala/poolmate/DecksView.scala
+++ js/src/main/scala/poolmate/DecksView.scala
@@ -7,7 +7,13 @@ import Validator.*
 
 object DecksView extends View:
   def apply(poolId: Long, model: Model[Deck], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case DecksListed(decks: List[Deck]) =>
+          clearErrors()
+          model.setEntities(decks)
+        case _ => log(s"Decks -> handler failed: $event")
 
     div(
       
