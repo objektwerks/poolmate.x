3bf4939262544a4bca0377d198856ae99fd823b4
objektwerks <objektwerks@runbox.com>
2023-03-12 08:55:21 -0400

    added handler to heater settings view

--- js/src/main/scala/poolmate/HeaterSettingsView.scala
+++ js/src/main/scala/poolmate/HeaterSettingsView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object HeaterSettingsView extends View:
   def apply(poolId: Long, heaterId: Long, model: Model[HeaterSetting], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case HeaterSettingsListed(heatersettings: List[HeaterSetting]) =>
+          clearErrors()
+          model.setEntities(heatersettings)
+        case _ => log(s"TimerSettings -> handler failed: $event")
 
     div(
       
