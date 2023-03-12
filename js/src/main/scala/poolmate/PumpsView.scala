33830331e3fbb60580e6609a76849a7baa486ea9
objektwerks <objektwerks@runbox.com>
2023-03-12 08:22:40 -0400

    removed import

--- js/src/main/scala/poolmate/PumpsView.scala
+++ js/src/main/scala/poolmate/PumpsView.scala
@@ -3,7 +3,6 @@ package poolmate
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Validator.*
 
 object PumpsView extends View:
   def apply(poolId: Long, model: Model[Pump], license: String): HtmlElement =
