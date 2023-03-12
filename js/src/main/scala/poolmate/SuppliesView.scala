6246830b84ec338670dcab9bca1c2a9dda90f980
objektwerks <objektwerks@runbox.com>
2023-03-12 08:34:51 -0400

    added handler to supplies view

--- js/src/main/scala/poolmate/SuppliesView.scala
+++ js/src/main/scala/poolmate/SuppliesView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object SuppliesView extends View:
   def apply(poolId: Long, model: Model[Supply], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case SuppliesListed(supplies: List[Supply]) =>
+          clearErrors()
+          model.setEntities(supplies)
+        case _ => log(s"Supplies -> handler failed: $event")
 
     div(
       
