454545d64b578303aaa16c57fcff574a93af8479
objektwerks <objektwerks@runbox.com>
2023-03-12 08:31:53 -0400

    added handler to heaters view

--- js/src/main/scala/poolmate/HeatersView.scala
+++ js/src/main/scala/poolmate/HeatersView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object HeatersView extends View:
   def apply(poolId: Long, model: Model[Heater], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case HeatersListed(heaters: List[Heater]) =>
+          clearErrors()
+          model.setEntities(heaters)
+        case _ => log(s"Heaters -> handler failed: $event")
 
     div(
       
