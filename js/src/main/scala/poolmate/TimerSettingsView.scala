252f433081cb87a815645ff1685d11c3293fb7fb
objektwerks <objektwerks@runbox.com>
2023-03-12 08:41:39 -0400

    removed import

--- js/src/main/scala/poolmate/TimerSettingsView.scala
+++ js/src/main/scala/poolmate/TimerSettingsView.scala
@@ -3,7 +3,6 @@ package poolmate
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Validator.*
 
 object TimerSettingsView extends View:
   def apply(poolId: Long, model: Model[TimerSetting], license: String): HtmlElement =
