61e6bd424b1a9b86d2bb7eb561b8dd7c49717724
objektwerks <objektwerks@runbox.com>
2023-03-12 08:42:35 -0400

    added handler to timer settings

--- js/src/main/scala/poolmate/TimerSettingsView.scala
+++ js/src/main/scala/poolmate/TimerSettingsView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object TimerSettingsView extends View:
   def apply(poolId: Long, model: Model[TimerSetting], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case TimerSettingsListed(timerSettings: List[TimerSetting]) =>
+          clearErrors()
+          model.setEntities(timerSettings)
+        case _ => log(s"TimerSettings -> handler failed: $event")
 
     div(
       
