b3f1e8f452c8dea60f92a0eb01c0b8ce4670378c
objektwerks <objektwerks@runbox.com>
2023-03-12 08:01:37 -0400

    added handler to surfaces view

--- js/src/main/scala/poolmate/SurfacesView.scala
+++ js/src/main/scala/poolmate/SurfacesView.scala
@@ -7,7 +7,13 @@ import Validator.*
 
 object SurfacesView extends View:
   def apply(poolId: Long, model: Model[Surface], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case SurfacesListed(surfaces: List[Surface]) =>
+          clearErrors()
+          model.setEntities(surfaces)
+        case _ => log(s"Surfaces -> handler failed: $event")
 
     div(
       
