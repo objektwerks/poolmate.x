0a7381d85990b803cf734681458901b7b2b57fc1
objektwerks <objektwerks@runbox.com>
2023-03-12 08:23:33 -0400

    added handler to pumps view

--- js/src/main/scala/poolmate/PumpsView.scala
+++ js/src/main/scala/poolmate/PumpsView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object PumpsView extends View:
   def apply(poolId: Long, model: Model[Pump], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case PumpsListed(pumps: List[Pump]) =>
+          clearErrors()
+          model.setEntities(pumps)
+        case _ => log(s"Pumps -> handler failed: $event")
 
     div(
       
