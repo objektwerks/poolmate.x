ba6486d7b0949c37c70356bdc8c9e1492a7baf22
objektwerks <objektwerks@runbox.com>
2023-03-12 08:21:45 -0400

    removed import

--- js/src/main/scala/poolmate/CleaningsView.scala
+++ js/src/main/scala/poolmate/CleaningsView.scala
@@ -3,7 +3,6 @@ package poolmate
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Validator.*
 
 object CleaningsView extends View:
   def apply(poolId: Long, model: Model[Cleaning], license: String): HtmlElement =
