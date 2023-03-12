35ac0e74c57f7b12eabd351521f6add3cbc8489d
objektwerks <objektwerks@runbox.com>
2023-03-12 08:38:14 -0400

    added handler to repairs view

--- js/src/main/scala/poolmate/RepairsView.scala
+++ js/src/main/scala/poolmate/RepairsView.scala
@@ -6,7 +6,13 @@ import Component.*
 
 object RepairsView extends View:
   def apply(poolId: Long, model: Model[Repair], license: String): HtmlElement =
-    def handler(event: Event): Unit = ???
+    def handler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(cause)
+        case RepairsListed(repairs: List[Repair]) =>
+          clearErrors()
+          model.setEntities(repairs)
+        case _ => log(s"Repairs -> handler failed: $event")
 
     div(
       
