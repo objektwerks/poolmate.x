9595e6c08baba159290ece63387c3c202316d28d
objektwerks <objektwerks@runbox.com>
2023-03-12 08:48:03 -0400

    refactored page router for timer and heater settings

--- js/src/main/scala/poolmate/PageRouter.scala
+++ js/src/main/scala/poolmate/PageRouter.scala
@@ -184,14 +184,14 @@ object PageRouter:
     .collectStatic(TimersPage) { TimersView(Model.pools.selectedEntityVar.now().id, Model.timers, Model.license) }
     .collect[TimerPage] { page => TimerView(Model.timers.setSelectedEntityById(page.id), Model.license) }
 
-    .collectStatic(TimerSettingsPage) { TimerSettingsView(Model.pools.selectedEntityVar.now().id, Model.timersettings, Model.license) }
-    .collect[TimerSettingPage] { page => TimerSettingView(Model.timers.selectedEntityVar.now().id, Model.timersettings.setSelectedEntityById(page.id), Model.license) }
+    .collectStatic(TimerSettingsPage) { TimerSettingsView(Model.timers.selectedEntityVar.now().id, Model.timersettings, Model.license) }
+    .collect[TimerSettingPage] { page => TimerSettingView(Model.timersettings.selectedEntityVar.now().id, Model.timersettings.setSelectedEntityById(page.id), Model.license) }
 
     .collectStatic(HeatersPage) { HeatersView(Model.pools.selectedEntityVar.now().id, Model.heaters, Model.license) }
     .collect[HeaterPage] { page => HeaterView(Model.heaters.setSelectedEntityById(page.id), Model.license) }
 
-    .collectStatic(HeaterSettingsPage) { HeaterSettingsView(Model.pools.selectedEntityVar.now().id, Model.heatersettings, Model.license) }
-    .collect[HeaterSettingPage] { page => HeaterSettingView(Model.heaters.selectedEntityVar.now().id, Model.heatersettings.setSelectedEntityById(page.id), Model.license) }
+    .collectStatic(HeaterSettingsPage) { HeaterSettingsView(Model.heaters.selectedEntityVar.now().id, Model.heatersettings, Model.license) }
+    .collect[HeaterSettingPage] { page => HeaterSettingView(Model.heatersettings.selectedEntityVar.now().id, Model.heatersettings.setSelectedEntityById(page.id), Model.license) }
 
     .collectStatic(CleaningsPage) { CleaningsView(Model.pools.selectedEntityVar.now().id, Model.cleanings, Model.license) }
     .collect[CleaningPage] { page => CleaningView(Model.cleanings.setSelectedEntityById(page.id), Model.license) }
--- js/src/main/scala/poolmate/TimerSettingsView.scala
+++ js/src/main/scala/poolmate/TimerSettingsView.scala
@@ -5,7 +5,7 @@ import com.raquo.laminar.api.L.*
 import Component.*
 
 object TimerSettingsView extends View:
-  def apply(poolId: Long, model: Model[TimerSetting], license: String): HtmlElement =
+  def apply(timerId: Long, model: Model[TimerSetting], license: String): HtmlElement =
     def handler(event: Event): Unit =
       event match
         case Fault(cause, _) => emitError(cause)
@@ -15,5 +15,38 @@ object TimerSettingsView extends View:
         case _ => log(s"TimerSettings -> handler failed: $event")
 
     div(
-      
+      bar(
+        btn("Timer").amend {
+          onClick --> { _ =>
+            log("TimerSettings -> Pool menu item onClick")
+            route(TimerPage(timerId))
+          }
+        }      
+      ),
+      div(
+        onLoad --> { _ => 
+          val command = ListTimerSettings(license, timerId)
+          call(command, handler)
+        },
+        hdr("TimerSettings"),
+        err(errorBus),
+        listview(
+          split(model.entitiesVar, (id: Long) => TimerSettingPage(timerId))
+        )
+      ),
+      cbar(
+        btn("New").amend {
+          onClick --> { _ =>
+            log(s"Timers -> New button onClick")
+            route(TimerPage(poolId, model.selectedEntityVar.now().id))
+          }
+        },        
+        btn("Refresh").amend {
+          onClick --> { _ =>
+            log(s"Timers -> Refresh button onClick")
+            val command = ListTimers(license, poolId)
+            call(command, handler)
+          }
+        }
+      )
     )
