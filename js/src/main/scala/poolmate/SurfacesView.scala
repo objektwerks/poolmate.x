e10e4db91a95ccefc690c26a1072f44b64dd9154
objektwerks <objektwerks@runbox.com>
2023-03-12 08:20:48 -0400

    removed import

--- js/src/main/scala/poolmate/SurfacesView.scala
+++ js/src/main/scala/poolmate/SurfacesView.scala
@@ -3,7 +3,6 @@ package poolmate
 import com.raquo.laminar.api.L.*
 
 import Component.*
-import Validator.*
 
 object SurfacesView extends View:
   def apply(poolId: Long, model: Model[Surface], license: String): HtmlElement =
