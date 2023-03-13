e671b54ae71a5e10bd04a39d918cd6442d690ff2
objektwerks <objektwerks@runbox.com>
2023-03-13 15:42:07 -0400

    added update handler to heater view

--- js/src/main/scala/poolmate/HeaterView.scala
+++ js/src/main/scala/poolmate/HeaterView.scala
@@ -17,7 +17,13 @@ object HeaterView extends View:
           route(HeatersPage)
         case _ => log(s"Heater -> add handler failed: $event")
 
-    def updateHandler(event: Event): Unit = ???
+    def updateHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Update heater failed: $cause")
+        case Updated(_) =>
+          clearErrors()
+          route(HeatersPage)
+        case _ => log(s"Heater -> update handler failed: $event")
 
     div(
       
