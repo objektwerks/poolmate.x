4e8ef336ab0617238f674cb490aedea366fca11e
objektwerks <objektwerks@runbox.com>
2023-03-13 15:40:41 -0400

    added add handler to heater view

--- js/src/main/scala/poolmate/HeaterView.scala
+++ js/src/main/scala/poolmate/HeaterView.scala
@@ -8,7 +8,14 @@ import Validator.*
 
 object HeaterView extends View:
   def apply(model: Model[Heater], license: String): HtmlElement =
-    def addHandler(event: Event): Unit = ???
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add heater failed: $cause")
+        case HeaterAdded(heater) =>
+          clearErrors()
+          model.addEntity(heater)
+          route(HeatersPage)
+        case _ => log(s"Heater -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit = ???
 
