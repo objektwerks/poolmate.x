19ad1216370d7ba9adfc4ef5b105c37ad7fe5dc9
objektwerks <objektwerks@runbox.com>
2023-03-13 15:46:09 -0400

    added div to heater view

--- js/src/main/scala/poolmate/HeaterView.scala
+++ js/src/main/scala/poolmate/HeaterView.scala
@@ -26,5 +26,60 @@ object HeaterView extends View:
         case _ => log(s"Heater -> update handler failed: $event")
 
     div(
-      
+      bar(
+        btn("Heaters").amend {
+          onClick --> { _ =>
+            log("Heater -> Heaters menu item onClick")
+            route(HeatersPage)
+          }
+        },
+      ),
+      hdr("Heater"),
+      err(errorBus),
+      lbl("Installed"),
+      date.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(heater => localDateOfLongToString(heater.installed)),
+          onInput.mapToValue.filter(_.nonEmpty) --> { installed =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(installed = localDateOfStringToLong(installed)) )
+          }
+        )
+      },
+      lbl("Model"),
+      txt.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(_.model),
+          onChange.mapToValue.filter(_.nonEmpty) --> { m =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(model = m) )
+          }
+        )
+      },
+      lbl("Cost"),
+      int.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(_.cost.toString),
+          onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { cost =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(cost = cost) )
+          }
+        )
+      },
+      cbar(
+        btn("Add").amend {
+          disabled <-- model.selectedEntityVar.signal.map { heater => !(heater.id.isZero && heater.isValid) }
+          onClick --> { _ =>
+            log(s"Heater -> Add onClick")
+            val command = AddHeater(license, model.selectedEntityVar.now())
+            call(command, addHandler)
+
+          }
+        },
+        btn("Update").amend {
+          disabled <-- model.selectedEntityVar.signal.map { heater => !(heater.id.isGreaterThanZero && heater.isValid) }
+          onClick --> { _ =>
+            log(s"Heater -> Update onClick")
+            val command = UpdateHeater(license, model.selectedEntityVar.now())
+            call(command, updateHandler)
+          }
+        }
+      )
     )
