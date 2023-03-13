1c73506da89f96e83304e1feb16adf4680a8472a
objektwerks <objektwerks@runbox.com>
2023-03-13 09:14:55 -0400

    added add handler to surface view

--- js/src/main/scala/poolmate/SurfaceView.scala
+++ js/src/main/scala/poolmate/SurfaceView.scala
@@ -7,7 +7,14 @@ import Validator.*
 
 object SurfaceView extends View:
   def apply(model: Model[Surface], license: String): HtmlElement =
-    def addHandler(event: Event): Unit = ???
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add surface failed: $cause")
+        case SurfaceAdded(surface) =>
+          clearErrors()
+          model.addEntity(surface)
+          route(SurfacesPage)
+        case _ => log(s"Surface -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit = ???
 
