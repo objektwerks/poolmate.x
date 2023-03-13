cf102dec87e392d8a80f3314b54adac48de103a2
objektwerks <objektwerks@runbox.com>
2023-03-13 15:18:27 -0400

    added update handler to deck view

--- js/src/main/scala/poolmate/DeckView.scala
+++ js/src/main/scala/poolmate/DeckView.scala
@@ -10,14 +10,20 @@ object DeckView extends View:
   def apply(model: Model[Deck], license: String): HtmlElement =
     def addHandler(event: Event): Unit =
       event match
-        case Fault(cause, _) => emitError(s"Add surface failed: $cause")
+        case Fault(cause, _) => emitError(s"Add deck failed: $cause")
         case DeckAdded(deck) =>
           clearErrors()
           model.addEntity(deck)
           route(DecksPage)
         case _ => log(s"Deck -> add handler failed: $event")
 
-    def updateHandler(event: Event): Unit = ???
+    def updateHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Update deck failed: $cause")
+        case Updated(_) =>
+          clearErrors()
+          route(DecksPage)
+        case _ => log(s"Deck -> update handler failed: $event")
 
     div(
       
