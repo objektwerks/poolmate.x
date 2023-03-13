ebe7f604b78db41de2a25d5a92b351cb7a0f361e
objektwerks <objektwerks@runbox.com>
2023-03-13 15:38:05 -0400

    added div to timer view

--- js/src/main/scala/poolmate/TimerView.scala
+++ js/src/main/scala/poolmate/TimerView.scala
@@ -26,5 +26,60 @@ object TimerView extends View:
         case _ => log(s"Timer -> update handler failed: $event")
 
     div(
-      
+      bar(
+        btn("Timers").amend {
+          onClick --> { _ =>
+            log("Timer -> Timers menu item onClick")
+            route(TimersPage)
+          }
+        },
+      ),
+      hdr("Timer"),
+      err(errorBus),
+      lbl("Installed"),
+      date.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(timer => localDateOfLongToString(timer.installed)),
+          onInput.mapToValue.filter(_.nonEmpty) --> { installed =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(installed = localDateOfStringToLong(installed)) )
+          }
+        )
+      },
+      lbl("Model"),
+      txt.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(_.model),
+          onChange.mapToValue.filter(_.nonEmpty) --> { m =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(model = m) )
+          }
+        )
+      },
+      lbl("Cost"),
+      int.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(_.cost.toString),
+          onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { cost =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(cost = cost) )
+          }
+        )
+      },
+      cbar(
+        btn("Add").amend {
+          disabled <-- model.selectedEntityVar.signal.map { timer => !(timer.id.isZero && timer.isValid) }
+          onClick --> { _ =>
+            log(s"Timer -> Add onClick")
+            val command = AddTimer(license, model.selectedEntityVar.now())
+            call(command, addHandler)
+
+          }
+        },
+        btn("Update").amend {
+          disabled <-- model.selectedEntityVar.signal.map { timer => !(timer.id.isGreaterThanZero && timer.isValid) }
+          onClick --> { _ =>
+            log(s"Timer -> Update onClick")
+            val command = UpdateTimer(license, model.selectedEntityVar.now())
+            call(command, updateHandler)
+          }
+        }
+      )
     )
