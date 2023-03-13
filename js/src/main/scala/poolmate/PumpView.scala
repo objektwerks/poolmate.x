2d8cbdc3a22ce0a6e5703740c6d2d18c39242171
objektwerks <objektwerks@runbox.com>
2023-03-13 15:24:08 -0400

    added an add handler to pump view

--- js/src/main/scala/poolmate/PumpView.scala
+++ js/src/main/scala/poolmate/PumpView.scala
@@ -8,7 +8,14 @@ import Validator.*
 
 object PumpView extends View:
   def apply(model: Model[Pump], license: String): HtmlElement =
-    def addHandler(event: Event): Unit = ???
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add pump failed: $cause")
+        case PumpAdded(pump) =>
+          clearErrors()
+          model.addEntity(pump)
+          route(PumpsPage)
+        case _ => log(s"Pump -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit = ???
 
