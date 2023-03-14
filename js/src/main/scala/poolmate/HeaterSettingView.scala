2ea73dcf93857200bede4bc2bd096eefbd68c40a
objektwerks <objektwerks@runbox.com>
2023-03-14 09:04:09 -0400

    added div to heater setting

--- js/src/main/scala/poolmate/HeaterSettingView.scala
+++ js/src/main/scala/poolmate/HeaterSettingView.scala
@@ -14,7 +14,7 @@ object HeaterSettingView extends View:
         case HeaterSettingAdded(heatersetting) =>
           clearErrors()
           model.addEntity(heatersetting)
-          route(HeaterPage(heaterId))
+          route(HeaterSettingsPage)
         case _ => log(s"HeaterSetting -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit =
@@ -22,9 +22,64 @@ object HeaterSettingView extends View:
         case Fault(cause, _) => emitError(s"Update heater setting failed: $cause")
         case Updated(_) =>
           clearErrors()
-          route(HeaterPage(heaterId))
+          route(HeaterSettingsPage)
         case _ => log(s"HeaterSetting -> update handler failed: $event")
 
     div(
-      
+      bar(
+        btn("Heater Settings").amend {
+          onClick --> { _ =>
+            log("HeaterSetting -> Heater Settings menu item onClick")
+            route(HeaterSettingsPage)
+          }
+        },
+      ),
+      hdr("Heater Setting"),
+      err(errorBus),
+      lbl("Temperature"),
+      int.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(_.temp.toString),
+          onInput.mapToValue.filter(_.toIntOption.nonEmpty).map(_.toInt) --> { temp =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(temp = temp) )
+          }
+        )
+      },
+      lbl("Date On"),
+      date.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(heatersetting => localDateOfLongToString(heatersetting.dateOn)),
+          onInput.mapToValue.filter(_.nonEmpty) --> { dateOn =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(dateOn = localDateOfStringToLong(dateOn)) )
+          }
+        )
+      },
+      lbl("Date Off"),
+      date.amend {
+        controlled(
+          value <-- model.selectedEntityVar.signal.map(heatersetting => localDateOfLongToString(heatersetting.dateOff)),
+          onInput.mapToValue.filter(_.nonEmpty) --> { dateOff =>
+            model.updateSelectedEntity( model.selectedEntityVar.now().copy(dateOff = localDateOfStringToLong(dateOff)) )
+          }
+        )
+      },
+      cbar(
+        btn("Add").amend {
+          disabled <-- model.selectedEntityVar.signal.map { heatersetting => !(heatersetting.id.isZero && heatersetting.isValid) }
+          onClick --> { _ =>
+            log(s"HeaterSetting -> Add onClick")
+            val command = AddHeaterSetting(license, model.selectedEntityVar.now())
+            call(command, addHandler)
+
+          }
+        },
+        btn("Update").amend {
+          disabled <-- model.selectedEntityVar.signal.map { heater => !(heater.id.isGreaterThanZero && heater.isValid) }
+          onClick --> { _ =>
+            log(s"HeaterSetting -> Update onClick")
+            val command = UpdateHeaterSetting(license, model.selectedEntityVar.now())
+            call(command, updateHandler)
+          }
+        }
+      )
     )
