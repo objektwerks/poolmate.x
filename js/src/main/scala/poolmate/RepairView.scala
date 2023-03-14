0bb1f8bba6b27591f177942f6dc0bd138a4e2e51
objektwerks <objektwerks@runbox.com>
2023-03-14 09:48:28 -0400

    added add handler to repair view

--- js/src/main/scala/poolmate/RepairView.scala
+++ js/src/main/scala/poolmate/RepairView.scala
@@ -8,7 +8,14 @@ import Validator.*
 
 object RepairView extends View:
   def apply(model: Model[Repair], license: String): HtmlElement =
-    def addHandler(event: Event): Unit = ???
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add repair failed: $cause")
+        case RepairAdded(repair) =>
+          clearErrors()
+          model.addEntity(repair)
+          route(RepairsPage)
+        case _ => log(s"Repair -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit = ???
 
