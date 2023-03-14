c7b204b0a83699c0b9d63ef415a44d0428f84682
objektwerks <objektwerks@runbox.com>
2023-03-14 10:03:58 -0400

    added dashboard model to model

--- js/src/main/scala/poolmate/Model.scala
+++ js/src/main/scala/poolmate/Model.scala
@@ -2,9 +2,12 @@ package poolmate
 
 import com.raquo.laminar.api.L.*
 
+import java.text.NumberFormat
+
 import org.scalajs.dom.console.log
 
 import Entity.given
+import Measurement.*
 
 object Model:
   val emailAddressVar = Var("")
@@ -26,6 +29,90 @@ object Model:
   val chemicals = Model[Chemical](Var(List.empty[Chemical]), Var(Chemical()), Chemical(), chemicalOrdering)
   val supplies = Model[Supply](Var(List.empty[Supply]), Var(Supply()), Supply(), supplyOrdering)
   val repairs = Model[Repair](Var(List.empty[Repair]), Var(Repair()), Repair(), repairOrdering)
+
+  measurements
+    .entitiesVar
+    .toObservable
+    .changes
+    .collect( _ =>
+      log(s"dashboard calculating ...")
+      dashboard()
+      log(s"dashboard calculated.")
+    )
+
+  val currentTotalChlorine = Var(0)
+  val averageTotalChlorine = Var(0)
+  def totalChlorineInRange(value: Int): Boolean = totalChlorineRange.contains(value)
+
+  val currentFreeChlorine = Var(0)
+  val averageFreeChlorine = Var(0)
+  def freeChlorineInRange(value: Int): Boolean = freeChlorineRange.contains(value)
+
+  val currentCombinedChlorine = Var(0.0)
+  val averageCombinedChlorine = Var(0.0)
+  def combinedChlorineInRange(value: Double): Boolean = combinedChlorineRange.contains(value)
+
+  val currentPh = Var(0.0)
+  val averagePh = Var(0.0)
+  def phInRange(value: Double): Boolean = phRange.contains(value)
+
+  val currentCalciumHardness = Var(0)
+  val averageCalciumHardness = Var(0)
+  def calciumHardnessInRange(value: Int): Boolean = calciumHardnessRange.contains(value)
+
+  val currentTotalAlkalinity = Var(0)
+  val averageTotalAlkalinity = Var(0)
+  def totalAlkalinityInRange(value: Int): Boolean = totalAlkalinityRange.contains(value)
+
+  val currentCyanuricAcid = Var(0)
+  val averageCyanuricAcid = Var(0)
+  def cyanuricAcidInRange(value: Int): Boolean = cyanuricAcidRange.contains(value)
+
+  val currentTotalBromine = Var(0)
+  val averageTotalBromine = Var(0)
+  def totalBromineInRange(value: Int): Boolean = totalBromineRange.contains(value)
+
+  val currentSalt = Var(0)
+  val averageSalt = Var(0)
+  def saltInRange(value: Int): Boolean = saltRange.contains(value)
+
+  val currentTemperature = Var(0)
+  val averageTemperature = Var(0)
+  def temperatureInRange(value: Int): Boolean = temperatureRange.contains(value)
+
+  private def dashboard(): Unit =
+    val numberFormat = NumberFormat.getNumberInstance()
+    numberFormat.setMaximumFractionDigits(1)
+    measurements.entitiesVar.now().foreach { measurement =>
+      calculateCurrentMeasurements(measurement, numberFormat)
+      calculateAverageMeasurements(numberFormat)
+    }
+
+  private def calculateCurrentMeasurements(measurement: Measurement, numberFormat: NumberFormat): Unit =
+    currentTotalChlorine.set(measurement.totalChlorine)
+    currentFreeChlorine.set(measurement.freeChlorine)
+    currentCombinedChlorine.set(numberFormat.format( measurement.combinedChlorine ).toDouble)
+    currentPh.set(numberFormat.format( measurement.ph ).toDouble)
+    currentCalciumHardness.set(measurement.calciumHardness)
+    currentTotalAlkalinity.set(measurement.totalAlkalinity)
+    currentCyanuricAcid.set(measurement.cyanuricAcid)
+    currentTotalBromine.set(measurement.totalBromine)
+    currentSalt.set(measurement.salt)
+    currentTemperature.set(measurement.temperature)
+
+  private def calculateAverageMeasurements(numberFormat: NumberFormat): Unit =
+    val measurementz = measurements.entitiesVar.now()
+    val count = measurementz.length
+    averageTotalChlorine.set(measurementz.map(_.totalChlorine).sum / count)
+    averageFreeChlorine.set(measurementz.map(_.freeChlorine).sum / count)
+    averageCombinedChlorine.set(numberFormat.format( measurementz.map(_.combinedChlorine).sum / count ).toDouble)
+    averagePh.set(numberFormat.format( measurementz.map(_.ph).sum / count ).toDouble)
+    averageCalciumHardness.set(measurementz.map(_.calciumHardness).sum / count)
+    averageTotalAlkalinity.set(measurementz.map(_.totalAlkalinity).sum / count)
+    averageCyanuricAcid.set(measurementz.map(_.cyanuricAcid).sum / count)
+    averageTotalBromine.set(measurementz.map(_.totalBromine).sum / count)
+    averageSalt.set(measurementz.map(_.salt).sum / count)
+    averageTemperature.set(measurementz.map(_.temperature).sum / count)
 
 final case class Model[E <: Entity](entitiesVar: Var[List[E]],
                                     selectedEntityVar: Var[E],
