de37990c3eb35253b58c4f32f261d398d939ff9e
objektwerks <objektwerks@runbox.com>
2023-03-14 08:24:47 -0400

    added update handler to timer setting view

--- js/src/main/scala/poolmate/TimerSettingView.scala
+++ js/src/main/scala/poolmate/TimerSettingView.scala
@@ -9,14 +9,20 @@ object TimerSettingView extends View:
   def apply(timerId: Long, model: Model[TimerSetting], license: String): HtmlElement =
     def addHandler(event: Event): Unit =
       event match
-        case Fault(cause, _) => emitError(s"Add timer failed: $cause")
+        case Fault(cause, _) => emitError(s"Add timer setting failed: $cause")
         case TimerSettingAdded(timersetting) =>
           clearErrors()
           model.addEntity(timersetting)
           route(TimerPage(timerId))
         case _ => log(s"TimerSetting -> add handler failed: $event")
 
-    def updateHandler(event: Event): Unit = ???
+    def updateHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Update timer setting failed: $cause")
+        case Updated(_) =>
+          clearErrors()
+          route(TimerPage(timerId))
+        case _ => log(s"TimerSetting -> update handler failed: $event")
 
     div(
       
