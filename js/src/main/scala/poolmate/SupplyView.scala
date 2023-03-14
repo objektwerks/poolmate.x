6fa4eaad4230c505fee67266ff493ddb5ead79f7
objektwerks <objektwerks@runbox.com>
2023-03-14 09:34:35 -0400

    added add handler to supply view

--- js/src/main/scala/poolmate/SupplyView.scala
+++ js/src/main/scala/poolmate/SupplyView.scala
@@ -8,7 +8,14 @@ import Validator.*
 
 object SupplyView extends View:
   def apply(model: Model[Supply], license: String): HtmlElement =
-    def addHandler(event: Event): Unit = ???
+    def addHandler(event: Event): Unit =
+      event match
+        case Fault(cause, _) => emitError(s"Add supply failed: $cause")
+        case SupplyAdded(supply) =>
+          clearErrors()
+          model.addEntity(supply)
+          route(SuppliesPage)
+        case _ => log(s"Supply -> add handler failed: $event")
 
     def updateHandler(event: Event): Unit = ???
 
